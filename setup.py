dddc320b4d10c7e7abce698b8f78282546d1f6b3
thongdong7 <thongdong7@gmail.com>
2015-12-19 17:29:49 +0700

    update setup file

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-        name='pysub',
-        version='0.1.0.0',
+        name='subfind',
+        version='0.1.0.5',
         description='Subtitle crawler written in Python',
         author='Thong Dong',
         author_email='thongdong7@gmail.com',
-        install_requires=['lxml', 'clint'],
+        url='https://github.com/thongdong7/pysub',
+        packages=find_packages(exclude=["build", "dist", "tests*"]),
+        install_requires=['lxml', 'Distance==0.1.3', 'clint'],
         entry_points={
             'console_scripts': [
                 'subfind = pysub.cli:main',
             ],
-        }
+        },
+        classifiers=[
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Environment :: Console",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2.7",
+        ],
 )
