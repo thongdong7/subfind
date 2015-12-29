a8fc47edc9b904b81bd4353f4773d7e6f497ea86
thongdong7 <thongdong7@gmail.com>
2015-12-29 20:39:01 +0700

    update setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='subfind',
-        version='0.2.0.0',
+        version='1.0.0.0',
         description='Subtitle crawler written in Python',
         author='Thong Dong',
         author_email='thongdong7@gmail.com',
@@ -28,6 +28,11 @@ setup(
             "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
             "Environment :: Console",
             "Programming Language :: Python",
+            "Programming Language :: Python :: 2.6",
             "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3.2",
+            "Programming Language :: Python :: 3.3",
+            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3.5",
         ],
 )
