6c4aca1b293ecce098498f6fb6150ee0795bd489
thongdong7 <thongdong7@gmail.com>
2016-01-02 12:17:20 +0700

    add more testcase

--- tests/TestSubFind.py
+++ tests/TestSubFind.py
@@ -1,5 +1,4 @@
 from os import unlink
-
 from os.path import dirname, join, abspath, exists
 
 from subfind import SubFind
@@ -15,22 +14,29 @@ logging.basicConfig(level=logging.DEBUG)
 class SubFindTestCase(unittest.TestCase):
     def test_01(self):
         self.data_dir = abspath(join(dirname(__file__), 'data'))
-        m1_dir = join(self.data_dir, 'm1')
-        sub_file = join(m1_dir, 'Everest.2015.HC.1080p.HDRiP.x264.ShAaNiG.vi.srt')
-        if exists(sub_file):
-            unlink(sub_file)
 
-        sub_finder = SubFind(languages=['vi'], provider='subscene', force=True)
+        testcases = [
+            ('m1', ['vi'], 'Everest.2015.HC.1080p.HDRiP.x264.ShAaNiG.vi.srt', 106842),
+            ('m2', ['en'], 'Survivor.2014.1080p.BluRay.H264.AAC-RARBG.en.srt', 77028),
+        ]
 
-        for item in sub_finder.scan(m1_dir):
-            print(item)
+        for test_dir, languages, sub_file, expected_size in testcases:
+            m1_dir = join(self.data_dir, test_dir)
+            sub_file_path = join(m1_dir, sub_file)
+            if exists(sub_file_path):
+                unlink(sub_file_path)
 
-        self.assertTrue(exists(sub_file))
+            sub_finder = SubFind(languages=languages, provider='subscene', force=True)
 
-        sub_size = len(get_file_content(sub_file))
-        self.assertEqual(106842, sub_size)
+            for item in sub_finder.scan(m1_dir):
+                print(item)
 
-        unlink(sub_file)
+            self.assertTrue(exists(sub_file_path))
+
+            sub_size = len(get_file_content(sub_file_path))
+            self.assertEqual(expected_size, sub_size)
+
+            unlink(sub_file_path)
 
 
 if __name__ == '__main__':
