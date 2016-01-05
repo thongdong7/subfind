957855d7e7ce82a4994e65ac4307e70f4ced011b
thongdong7 <thongdong7@gmail.com>
2016-01-05 20:59:49 +0700

    update setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='subfind',
-        version='2.0.0.0',
+        version='2.0.1.0',
         description='Subtitle crawler written in Python',
         author='Thong Dong',
         author_email='thongdong7@gmail.com',
