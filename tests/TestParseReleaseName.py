f3eb011a584fc5d6d848117a09b52e657d071133
Thong Dong <thongdong7@gmail.com>
2016-04-23 13:45:20 +0700

    release parser: fix year

---
+++ tests/TestParseReleaseName.py
@@ -0,0 +1,25 @@
+from subfind import parse_release_name
+
+__author__ = 'hiepsimu'
+import logging
+import unittest
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class ParseReleaseNameTestCase(unittest.TestCase):
+    def test_01(self):
+        testcases = [
+            ('Bad.Boys.II.2003.1080.BluRay.X264.YIFY', {'year': 2003, 'title_query': 'bad boys ii'})
+        ]
+
+        for release_name, expect_result in testcases:
+            ret = parse_release_name(release_name)
+            # print(ret)
+            for field_name in expect_result:
+                self.assertTrue(field_name in ret)
+                self.assertEqual(expect_result[field_name], ret[field_name])
+
+
+if __name__ == '__main__':
+    unittest.main()
