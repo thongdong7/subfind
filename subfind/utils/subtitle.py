242022d9d9d505de1fcac8ddc6c48497169c1d79
Thong Dong <thongdong7@gmail.com>
2016-04-24 09:08:12 +0700

    utils: get subtitle info from file name

--- subfind/utils/subtitle.py
+++ subfind/utils/subtitle.py
@@ -23,6 +23,10 @@ subtitle_extensions = set([
     "vtt"
 ])
 
+languages_code = set([
+    'en', 'vi'
+])
+
 
 def get_subtitle_ext(path):
     if not path:
@@ -40,3 +44,22 @@ def remove_subtitle(path, release_name, lang):
         sub_file = join(path, '%s.%s.%s' % (release_name, lang, ext))
         if exists(sub_file):
             os.unlink(sub_file)
+
+
+def get_subtitle_info(file_name):
+    if not file_name:
+        return None
+
+    tokens = file_name.split('.')
+    if len(tokens) < 2 or tokens[-1] not in subtitle_extensions:
+        # Not subtitle file
+        return None
+
+    ret = {
+        'ext': tokens[-1],
+    }
+
+    if tokens[-2] in languages_code:
+        ret['lang'] = tokens[-2]
+
+    return ret
