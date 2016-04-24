85f600f06fe6210fcd0de93079329456e1d84c8d
Thong Dong <thongdong7@gmail.com>
2016-04-24 17:50:04 +0700

    save config to file

--- subfind_web/main.py
+++ subfind_web/main.py
@@ -6,32 +6,19 @@ from subfind import parse_release_name, SubFind
 from subfind.event import EventManager
 from subfind_web.api import api
 from subfind_web.crossdomain import crossdomain
+from subfind_web.utils import save_config, get_config
 from subfind_web.validate import folder_validator, ValidatorManager
 
 app = Flask(__name__)
 
-languages = ['vi']
-providers = ['subscene']
-# providers = ['opensubtitles', 'subscene']
-force = False
-remove = False
-# ignore files less than 500 MB
-min_movie_size = 500 * 1024 * 1024
-max_sub = 5
-src_dirs = ["/data2/movies"]
-
-config = {
-    'src': src_dirs,
-    'lang': languages,
-    'force': force,
-    'remove': remove,
-    'max-sub': max_sub
-}
+config = get_config()
 
 event_manager = EventManager()
 
-sub_finder = SubFind(event_manager, languages=languages, provider_names=providers, force=force, remove=remove,
-                     min_movie_size=min_movie_size, max_sub=max_sub)
+sub_finder = SubFind(event_manager, languages=config['lang'],
+                     provider_names=config['providers'], force=config['force'],
+                     remove=config['remove'],
+                     min_movie_size=config['min-movie-size'], max_sub=config['max-sub'])
 
 data = []
 
@@ -70,16 +57,6 @@ def hello():
 @crossdomain(origin='*')
 @api
 def release():
-    # print(data)
-    # print(movie_requests)
-    #
-    # data = [
-    #     {'name': 'Avengers.Age.of.Ultron.2015.1080p.BluRay.x264.YIFY'}
-    # ]
-
-    # for item in data:
-    #     item.update(parse_release_name(item['name']))
-
     return data
 
 
@@ -124,28 +101,9 @@ def config_update():
                 tmp.remove(remove_value)
                 update[field_name] = sorted(list(tmp))
 
-    # add_src = request.args.get('src-$push')
-    # if add_src:
-    #     add_src_abs = abspath(add_src)
-    #     if not exists(add_src_abs):
-    #         raise APIError("Invalid folder %s" % add_src)
-    #
-    #     src = set(config['src'])
-    #     src.add(add_src_abs)
-    #     update['src'] = sorted(list(src))
-    #
-    # remove_value = request.args.get('src-$remove')
-    # if remove_value:
-    #     remove_src_abs = abspath(remove_value)
-    #     if not exists(remove_src_abs):
-    #         raise APIError("Invalid folder %s" % remove_value)
-    #
-    #     src = set(config['src'])
-    #     if remove_src_abs in src:
-    #         src.remove(remove_src_abs)
-    #         update['src'] = sorted(list(src))
-
     config.update(update)
+
+    save_config(config)
 
     if 'src' in update:
         build_data()
@@ -156,7 +114,7 @@ def config_update():
 @app.route("/release/scan-all")
 @crossdomain(origin='*')
 def release_scan_all():
-    sub_finder.scan(src_dirs)
+    sub_finder.scan(config['src'])
 
     build_data()
 
---
+++ subfind_web/utils.py
@@ -0,0 +1,37 @@
+import yaml
+from os.path import join, exists
+
+import os
+
+home_dir = os.path.expanduser('~')
+subfind_config_dir = join(home_dir, '.subfind')
+
+if not exists(subfind_config_dir):
+    os.makedirs(subfind_config_dir)
+
+config_path = join(subfind_config_dir, 'subfind.yml')
+
+default_config = {
+    'src': [],
+    'providers': ['opensubtitles', 'subscene'],
+    'lang': ['en'],
+    'force': False,
+    'remove': False,
+    # Get maximum 5 subtitles for each release
+    'max-sub': 5,
+    # ignore files less than 500 MB
+    'min-movie-size': 500 * 1024 * 1024,
+}
+
+
+def get_config():
+    if not exists(config_path):
+        save_config(default_config)
+
+    return yaml.load(open(config_path))
+
+
+def save_config(config):
+    content = yaml.safe_dump(config)
+
+    open(config_path, 'w').write(content)
