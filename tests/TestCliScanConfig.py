0bb15ea96fa0d6db58a768d62ba6d3bccde72ea8
Thong Dong <thongdong7@gmail.com>
2016-05-01 07:12:33 +0700

    update cli testcases

--- tests/TestCliScanConfig.py
+++ tests/TestCliScanConfig.py
@@ -19,12 +19,17 @@ class CliScanConfigTestCase(unittest.TestCase):
     def test_01(self):
         cli_dir = join(self.data_dir, 'cli1')
         config_file = join(cli_dir, 'subfind.yml')
-        movies_dir = [join(self.data_dir, 'm1'), join(self.data_dir, 'm2')]
+        movies_dir_name = ['m1', ',2']
+        movies_dir = []
+        for movie_dir_name in movies_dir_name:
+            movies_dir.append(join(self.data_dir, movie_dir_name))
+
         write_file_content(config_file, yaml.safe_dump({
             'src': movies_dir,
             'force': True,
             'lang': ['vi', 'en'],
-            'min-movie-size': 0
+            'min-movie-size': 0,
+            'providers': ['opensubtitles', 'subscene']
         }))
 
         scan_config(config_file)
