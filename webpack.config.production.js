9fe2362222010cdb21d616b7af85a938e60c4626
Thong Dong <thongdong7@gmail.com>
2016-05-01 20:25:02 +0700

    optimize css and js

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -19,12 +19,22 @@ config.output = {
 
 config.entry = {
   vendor: [
+    'admin-lte/bootstrap/css/bootstrap.css',
+    'font-awesome/css/font-awesome.min.css',
+    'ionicons/dist/css/ionicons.min.css',
+    'admin-lte/dist/css/AdminLTE.min.css',
+    'admin-lte/dist/css/skins/skin-blue.min.css',
     'jquery',
+    'admin-lte/bootstrap/js/bootstrap.js',
+    'admin-lte/dist/js/app.js',
+    'lodash',
     'babel-polyfill',
     'react',
     'react-addons-update',
     'react-router',
-    'react-typeahead'
+    'react-typeahead',
+
+    'react-dom'
   ],
   app: "./client/main.js",
 }
@@ -34,9 +44,14 @@ config.entry = {
 //}
 
 config.plugins = config.plugins.concat([
+  new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery"
+  }),
   new webpack.optimize.OccurenceOrderPlugin(true),
   new webpack.optimize.DedupePlugin(),
-  new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js"),
+
 //  new webpack.optimize.UglifyJsPlugin({
 //    output: {
 //      comments: false
@@ -46,6 +61,7 @@ config.plugins = config.plugins.concat([
 //      screw_ie8: true
 //    }
 //  }),
+  new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js"),
 //  new SaveAssetsJson({
 //    path: process.cwd(),
 //    filename: 'assets.json'
@@ -58,7 +74,13 @@ config.plugins = config.plugins.concat([
 ]);
 
 config.module.loaders = config.module.loaders.concat([
-  {test: /\.jsx?$/, loader: 'babel?presets[]=react,presets[]=es2015,presets[]=stage-0', exclude: /node_modules/}
+  {test: /\.jsx?$/, loader: 'babel?presets[]=react,presets[]=es2015,presets[]=stage-0', exclude: /node_modules/},
+  { test: /\.css$/, loader: "style-loader!css-loader" },
+  { test: /\.woff/, loader: "file-loader" },
+  { test: /\.eot/, loader: "file-loader" },
+  { test: /\.ttf/, loader: "file-loader" },
+  { test: /\.svg/, loader: "file-loader" },
+  { test: /\.jpg/, loader: "file-loader" },
 ]);
 
 module.exports = config;
