7749107ae4b13301513e1d30589f8a18c07fc328
Thong Dong <thongdong7@gmail.com>
2016-05-07 05:27:04 +0700

    infinite list

--- client/components/SFRelease/List.js
+++ client/components/SFRelease/List.js
@@ -9,23 +9,55 @@ import update from 'react-addons-update'
 import Switch from '../Switch'
 import SFReleaseFilter from './Filter'
 
+
+let onBottom
+
+window.onscroll = function(ev) {
+  if ((window.innerHeight + window.scrollY + 100) >= document.body.offsetHeight) {
+    if (onBottom) {
+      onBottom()
+    }
+    // console.log('bottom');
+  }
+};
+
 export default class SFReleaseList extends React.Component {
   constructor(props, context) {
     super(props, context)
 
     this.state = {
       data: [],
+      filteredData: [],
       current: null,
       connectionError: false,
       loading: false,
       filter: {
         // empty: false
-      }
+      },
+      page: 1,
+      maxPage: 1
     }
+
+    this.limit = 20
   }
 
   componentWillMount() {
+    onBottom = this.onBottom.bind(this)
+
     this.loadData()
+  }
+
+  onBottom() {
+    if (this.state.page >= this.state.maxPage) {
+      return
+    }
+
+    let nextPage = this.state.page + 1
+    this.setState({
+      page: nextPage,
+      filteredData: this.filterData(this.state.data, this.state.filter, nextPage)
+    })
+    // console.log('b', nextPage);
   }
 
   async loadData() {
@@ -33,9 +65,17 @@ export default class SFReleaseList extends React.Component {
       this.setState({loading: true})
 
       let data = await RestService.load('release')
-      // console.log(data)
 
-      this.setState({data: data, connectionError: false, loading: false})
+      let maxPage = data.length / this.limit
+
+      this.setState({
+        data: data,
+        filteredData: this.filterData(data, this.state.filter),
+        page: 1,
+        maxPage: maxPage,
+        connectionError: false,
+        loading: false
+      })
     } catch (promise) {
       if (promise.responseJSON && promise.responseJSON.code == 501) {
         // Missed config file
@@ -47,14 +87,25 @@ export default class SFReleaseList extends React.Component {
     }
   }
 
-  filter(item) {
-    if (this.state.filter.empty) {
+  filterData(data, filter, page) {
+    if (page == undefined) {
+      page = this.state.page
+    }
+
+    // console.log(page, this.limit);
+
+    return data.filter((item) => this.doFilter(item, filter))      .slice(0, page * this.limit)
+  }
+
+  doFilter(item, filter1) {
+    // console.log('a');
+    if (filter1.empty) {
       return _.isEmpty(item.subtitles)
     }
 
     for (let lang of ["en", "vi"]) {
       let fieldName = `lang_${lang}`
-      if (this.state.filter[fieldName]) {
+      if (filter1[fieldName]) {
         return !item.subtitles[lang] || item.subtitles[lang].length == 0
       }
     }
@@ -63,11 +114,13 @@ export default class SFReleaseList extends React.Component {
   }
 
   updateFilter(filter) {
-    this.setState({filter: filter})
+//    console.log('update filter', filter);
+    let filteredData = this.filteredData(this.state.data, filter)
+    this.setState({filter: filter, filteredData: filteredData})
   }
 
   render() {
-
+    // console.log('render', this.state.filteredData.length);
     return (
       <div className="box box-solid">
         <div className="box-header with-border">
@@ -89,7 +142,7 @@ export default class SFReleaseList extends React.Component {
         <div className="box-body">
           <SFReleaseFilter onChange={this.updateFilter.bind(this)}/>
 
-          {this.state.data.filter(this.filter.bind(this)).map((item, k) => {
+          {this.state.filteredData.map((item, k) => {
             let stateClass = ""
             if (_.isEmpty(item.subtitles)) {
               stateClass = " bg-warning"
