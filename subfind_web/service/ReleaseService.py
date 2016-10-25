d047ebdc9b652115a4a1e3d7e7cfed832736bd7e
An Vu <an.vu@sentifi.com>
2016-10-25 15:02:13 +0700

    add scan_all()

--- subfind_web/service/ReleaseService.py
+++ subfind_web/service/ReleaseService.py
@@ -2,5 +2,17 @@ from subfind_web.bootstrap import container
 
 
 class ReleaseService(object):
+    def __init__(self):
+        self.config = container.get('Config')
+        self.sub_finder = container.get('SubFinder')
+        self.data_provider = container.get('DataProvider')
+
     def list(self):
-        return container.get('DataProvider').data
+        return self.data_provider.data
+
+    def scan_all(self):
+        self.sub_finder.scan(self.config['src'])
+
+        self.data_provider.build_data()
+
+        return True
