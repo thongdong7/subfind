406d31377c377db505c2af56a530b82ea08115b1
An Vu <an.vu@sentifi.com>
2016-10-27 12:01:39 +0700

    use InputTextForm

--- src/ui/src/components/SFConfig/Index.js
+++ src/ui/src/components/SFConfig/Index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import MovieFolder from './MovieFolder'
 import InputEditable from '../InputEditable'
-import SimpleForm from '../SimpleForm'
 import * as tb from 'tb-react'
 import {configActions} from '../../actions/config'
 
@@ -101,114 +100,6 @@ class SFConfigIndex extends React.Component {
     // console.log('config', this.props.config);
     const config = this.props.config
 
-    let content = (
-      <div>
-        <div className="row">
-          <div className="col-sm-3"><strong>Movie Folders</strong></div>
-          <div className="col-sm-9">
-            {folders.map((folder, k) => {
-              return (
-                <MovieFolder
-                  src={folder}
-                  key={k}
-                >
-                  <tb.APIActionButton
-                    name="Remove"
-                    icon="trash"
-                    hideName
-                    type="danger"
-
-                    action={[configActions.removeField, 'src', folder]}
-                  />
-                </MovieFolder>
-              )
-            })}
-            <SimpleForm
-              action={[configActions.pushField, 'src']}
-            />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-3"><strong>Languages</strong></div>
-          <div className="col-sm-9">
-            {lang.map((item, k) => {
-              return (
-                <MovieFolder
-                  src={item}
-                  key={k}
-                >
-                  <tb.APIActionButton
-                    name="Remove"
-                    icon="trash"
-                    hideName
-                    type="danger"
-                    action={[configActions.removeField, 'lang', item]}
-                  />
-                </MovieFolder>
-              )
-            })}
-
-            <SimpleForm
-              action={[configActions.pushField, 'lang']}
-            />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-3"><strong>Providers</strong></div>
-          <div className="col-sm-9">
-            {this.providers.map((item, k) => {
-              let tmp = providers.filter(x => x === item.name)
-              let value = tmp.length === 1 ? true : false
-              // console.log(item.display_name, value);
-              return (
-                <div key={k}>
-                  <div className="col-sm-3">{item.display_name}</div>
-                  <div className="col-sm-9">
-                    <tb.Switch checked={value} onChange={(checked) => this.updateProvider(checked, item.name)}/>
-                  </div>
-                </div>
-              )
-            })}
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-3"><strong>Force download subtitle</strong></div>
-          <div className="col-sm-9">
-            <tb.Switch checked={force}
-              onChange={(checked) => this.updateSwitchField('force', checked)}/>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-3">
-            <strong>Remove old subtitles if not found new subtitle</strong>
-          </div>
-          <div className="col-sm-9">
-            <tb.Switch checked={remove}
-              onChange={(checked) => this.updateSwitchField('remove', checked)}/>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-3">
-            <strong>Min movie size (MB)</strong>
-            <div>(to ignore sample videos)</div>
-          </div>
-          <div className="col-sm-9">
-            <InputEditable name="min-movie-size" defaultValue={config['min-movie-size'] / mb}
-              onUpdate={this.onMinMovieSizeUpdate.bind(this)} />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-3">
-            <strong>Number subtitles</strong>
-          </div>
-          <div className="col-sm-9">
-            <InputEditable name="max-sub" defaultValue={config['max-sub']}
-              onUpdate={this.onMaxSubUpdate.bind(this)} />
-          </div>
-        </div>
-      </div>
-    )
-
     return (
       <div className="box box-solid">
         <div className="box-header with-border">
@@ -218,7 +109,111 @@ class SFConfigIndex extends React.Component {
           </h3>
         </div>
         <div className="box-body">
-          {content}
+        <div>
+          <div className="row">
+            <div className="col-sm-3"><strong>Movie Folders</strong></div>
+            <div className="col-sm-9">
+              {folders.map((folder, k) => {
+                return (
+                  <MovieFolder
+                    src={folder}
+                    key={k}
+                  >
+                    <tb.APIActionButton
+                      name="Remove"
+                      icon="trash"
+                      hideName
+                      type="danger"
+
+                      action={[configActions.removeField, 'src', folder]}
+                    />
+                  </MovieFolder>
+                )
+              })}
+              <tb.InputTextForm
+                action={[configActions.pushField, 'src']}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-3"><strong>Languages</strong></div>
+            <div className="col-sm-9">
+              {lang.map((item, k) => {
+                return (
+                  <MovieFolder
+                    src={item}
+                    key={k}
+                  >
+                    <tb.APIActionButton
+                      name="Remove"
+                      icon="trash"
+                      hideName
+                      type="danger"
+                      action={[configActions.removeField, 'lang', item]}
+                    />
+                  </MovieFolder>
+                )
+              })}
+
+              <tb.InputTextForm
+                action={[configActions.pushField, 'lang']}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-3"><strong>Providers</strong></div>
+            <div className="col-sm-9">
+              {this.providers.map((item, k) => {
+                let tmp = providers.filter(x => x === item.name)
+                let value = tmp.length === 1 ? true : false
+                // console.log(item.display_name, value);
+                return (
+                  <div key={k}>
+                    <div className="col-sm-3">{item.display_name}</div>
+                    <div className="col-sm-9">
+                      <tb.Switch checked={value} onChange={(checked) => this.updateProvider(checked, item.name)}/>
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-3"><strong>Force download subtitle</strong></div>
+            <div className="col-sm-9">
+              <tb.Switch checked={force}
+                onChange={(checked) => this.updateSwitchField('force', checked)}/>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-3">
+              <strong>Remove old subtitles if not found new subtitle</strong>
+            </div>
+            <div className="col-sm-9">
+              <tb.Switch checked={remove}
+                onChange={(checked) => this.updateSwitchField('remove', checked)}/>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-3">
+              <strong>Min movie size (MB)</strong>
+              <div>(to ignore sample videos)</div>
+            </div>
+            <div className="col-sm-9">
+              <InputEditable name="min-movie-size" defaultValue={config['min-movie-size'] / mb}
+                onUpdate={this.onMinMovieSizeUpdate.bind(this)} />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-3">
+              <strong>Number subtitles</strong>
+            </div>
+            <div className="col-sm-9">
+              <InputEditable name="max-sub" defaultValue={config['max-sub']}
+                onUpdate={this.onMaxSubUpdate.bind(this)} />
+            </div>
+          </div>
+        </div>
         </div>
       </div>
     )
