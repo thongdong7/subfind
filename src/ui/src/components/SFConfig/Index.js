0a58bfec64ae25abc003a305d3f181f7d961d294
An Vu <an.vu@sentifi.com>
2016-10-27 17:56:58 +0700

    refactor

--- src/ui/src/components/SFConfig/Index.js
+++ src/ui/src/components/SFConfig/Index.js
@@ -5,22 +5,23 @@ import {configActions} from '../../actions/config'
 
 let mb = 1024 * 1024
 
+const FormRow = ({children}) => {
+  return (
+    <div className="row">
+      <div className="col-sm-3">{children[0]}</div>
+      <div className="col-sm-9">{children[1]}</div>
+    </div>
+  )
+}
+
+const builtinProviders = [
+  {name: "opensubtitles", displayName: "Opensubtitles"},
+  {name: "subscene", displayName: "Subscene"},
+]
+
 class SFConfigIndex extends Component {
   static contextTypes = {
       router: PropTypes.object
-  }
-
-  constructor(props, context) {
-    super(props, context)
-
-    this.providers = [
-      {name: "opensubtitles", displayName: "Opensubtitles"},
-      {name: "subscene", displayName: "Subscene"},
-    ]
-  }
-
-  back() {
-    this.context.router.goBack()
   }
 
   render() {
@@ -34,126 +35,115 @@ class SFConfigIndex extends Component {
       <div className="box box-solid">
         <div className="box-header with-border">
           <h3 className="box-title">
-            <button className="btn btn-default" onClick={this.back.bind(this)}><i className="fa fa-arrow-left"></i> Back</button>
-            &nbsp;
+            <tb.BackButton />
           </h3>
         </div>
         <div className="box-body">
-        <div>
-          <div className="row">
-            <div className="col-sm-3"><strong>Movie Folders</strong></div>
-            <div className="col-sm-9">
-              {folders.map((folder, k) => {
-                return (
-                  <MovieFolder
-                    src={folder}
-                    key={k}
-                  >
-                    <tb.APIActionButton
-                      name="Remove"
-                      icon="trash"
-                      hideName
-                      type="danger"
+          <div>
+            <FormRow>
+              <strong>Movie Folders</strong>
+              <div>
+                {folders.map((folder, k) => {
+                  return (
+                    <MovieFolder
+                      src={folder}
+                      key={k}
+                    >
+                      <tb.APIActionButton
+                        name="Remove"
+                        icon="trash"
+                        hideName
+                        type="danger"
 
-                      action={[configActions.updateListField, 'src', folder, false]}
-                    />
-                  </MovieFolder>
-                )
-              })}
-              <tb.InputTextForm
-                actionFunc={(value) => [configActions.updateListField, 'src', value, true]}
-              />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-3"><strong>Languages</strong></div>
-            <div className="col-sm-9">
-              {languages.map((lang, k) => {
-                return (
-                  <MovieFolder
-                    src={lang}
-                    key={k}
-                  >
-                    <tb.APIActionButton
-                      name="Remove"
-                      icon="trash"
-                      hideName
-                      type="danger"
-
-                      action={[configActions.updateListField, 'lang', lang, false]}
-                    />
-                  </MovieFolder>
-                )
-              })}
-
-              <tb.InputTextForm
-                actionFunc={(value) => [configActions.updateListField, 'lang', value, true]}
-              />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-3"><strong>Providers</strong></div>
-            <div className="col-sm-9">
-              {this.providers.map(({name: providerName, displayName}, k) => {
-                let checked = providers.indexOf(providerName) >= 0
-                return (
-                  <div key={k}>
-                    <div className="col-sm-3">{displayName}</div>
-                    <div className="col-sm-9">
-                      <tb.APIActionSwitch
-                        checked={checked}
-                        action={[configActions.updateListField, 'providers', providerName]}
+                        action={[configActions.updateListField, 'src', folder, false]}
                       />
+                    </MovieFolder>
+                  )
+                })}
+                <tb.InputTextForm
+                  actionFunc={(value) => [configActions.updateListField, 'src', value, true]}
+                />
+              </div>
+            </FormRow>
+            <FormRow>
+              <strong>Languages</strong>
+              <div>
+                {languages.map((lang, k) => {
+                  return (
+                    <MovieFolder
+                      src={lang}
+                      key={k}
+                    >
+                      <tb.APIActionButton
+                        name="Remove"
+                        icon="trash"
+                        hideName
+                        type="danger"
+
+                        action={[configActions.updateListField, 'lang', lang, false]}
+                      />
+                    </MovieFolder>
+                  )
+                })}
+
+                <tb.InputTextForm
+                  actionFunc={(value) => [configActions.updateListField, 'lang', value, true]}
+                />
+              </div>
+            </FormRow>
+            <FormRow>
+              <strong>Providers</strong>
+              <div>
+                {builtinProviders.map(({name: providerName, displayName}, k) => {
+                  let checked = providers.indexOf(providerName) >= 0
+                  return (
+                    <div key={k}>
+                      <div className="col-sm-3">{displayName}</div>
+                      <div className="col-sm-9">
+                        <tb.APIActionSwitch
+                          checked={checked}
+                          action={[configActions.updateListField, 'providers', providerName]}
+                        />
+                      </div>
                     </div>
-                  </div>
-                )
-              })}
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-3"><strong>Force download subtitle</strong></div>
-            <div className="col-sm-9">
+                  )
+                })}
+              </div>
+            </FormRow>
+            <FormRow>
+              <strong>Force download subtitle</strong>
               <tb.APIActionSwitch
                 checked={force}
                 action={[configActions.updateField, 'force']}
               />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-3">
+            </FormRow>
+            <FormRow>
               <strong>Remove old subtitles if not found new subtitle</strong>
-            </div>
-            <div className="col-sm-9">
               <tb.APIActionSwitch
                 checked={remove}
                 action={[configActions.updateField, 'remove']}
               />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-3">
-              <strong>Min movie size (MB)</strong>
-              <div>(to ignore sample videos)</div>
-            </div>
-            <div className="col-sm-9">
-              <tb.InputInplace
-                value={config['min-movie-size'] / mb}
-                actionFunc={(value) => [configActions.updateField, 'min-movie-size', Number(value) * mb]}
-              />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-3">
+            </FormRow>
+            <FormRow>
+              <div>
+                <strong>Min movie size (MB)</strong>
+                <div>(to ignore sample videos)</div>
+              </div>
+              <div>
+                <tb.InputInplace
+                  value={config['min-movie-size'] / mb}
+                  actionFunc={(value) => [configActions.updateField, 'min-movie-size', Number(value) * mb]}
+                />
+              </div>
+            </FormRow>
+            <FormRow>
               <strong>Number subtitles</strong>
-            </div>
-            <div className="col-sm-9">
               <tb.InputInplace
                 value={config['max-sub']}
                 action={[configActions.updateField, 'max-sub']}
               />
-            </div>
+            </FormRow>
           </div>
-        </div>
         </div>
       </div>
     )
