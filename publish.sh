efc70a2cf11418c53f51035265f62a077d4df8cd
Thong Dong <thongdong7@gmail.com>
2016-10-28 08:44:27 +0700

    fix manifest

--- publish.sh
+++ publish.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-rm subfind_web/static/js/* -rf
+#rm subfind_web/static/js/* -rf
 
-echo Build js files...
-npm run dist
+#echo Build js files...
+#npm run dist
 
 echo Upload packages...
 python setup.py bdist_wheel --universal upload
@@ -13,7 +13,7 @@ python setup.py bdist_wheel --universal upload
 echo Build deb file
 rm deb_dist -rf
 
-python3 setup.py --command-packages=stdeb.command bdist_deb
-python setup.py --command-packages=stdeb.command bdist_deb
+#python3 setup.py --command-packages=stdeb.command bdist_deb
+#python setup.py --command-packages=stdeb.command bdist_deb
 
 #dpkg -i deb_dist/python3-subfind_4.5.0-1_all.deb
