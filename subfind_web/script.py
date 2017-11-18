44a9c6f8b92209d64e80af35e7ae9148341eeb3d
An Vu <you@example.com>
2017-11-18 16:05:51 +0700

    Appveyor (3)

--- subfind_web/script.py
+++ subfind_web/script.py
@@ -7,18 +7,7 @@ from subfind_web.app import start_web
 @click.option('--port', '-p', default=32500, help='Web port')
 @click.option('--dev', is_flag=True, help='Run in development mode')
 def run(port, dev):
-    # from tb_api.script import start
-    # web_dir = abspath(dirname(__file__))
-    #
-    # project_dir = join(web_dir, 'ui/build')
-    # config = Config(project_dir, debug=dev, port=port)
-    # config_files = [join(web_dir, 'conf/api.yml')]
-    # config.loader = LoaderIOC(config_files, module_suffix='Service')
-    #
-    # start(
-    #     config
-    # )
-
+    print('run111')
     start_web(port=port, debug=dev)
 
 
